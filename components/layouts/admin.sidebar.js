24ebc89e5c94cafb8b5c3ec46f3e835b7a46a94d
sangpra <sangpra4176@gmail.com>
2021-10-27 21:10:56 +0700

    v2

--- components/layouts/admin.sidebar.js
+++ components/layouts/admin.sidebar.js
@@ -9,7 +9,7 @@ const AdminSidebar = () => {
   <Link href='#'>
   <a className="brand-link">
     <Image src="/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{opacity: '.8'}} width={20} height={20} />
-    <span className="brand-text font-weight-light">Admin Panel</span>
+    <span className="brand-text font-weight-light">MorningMorning</span>
   </a>
   </Link>
   {/* Sidebar */}
